6ba5699e4c40a16952c9f85e0768598728eafdd6
Gao Xiang <gaoxiang06@kuaishou.com>
2022-11-19 23:04:42 +0800

    fix: cypress warning

--- src/components/actor/ActorDetail.cy.js
+++ src/components/actor/ActorDetail.cy.js
@@ -1,8 +1,8 @@
-import ActorDetail from './ActorDetail.vue'
+import ActorDetail from "./ActorDetail.vue";
 
-describe('<ActorDetail />', () => {
-  it('renders', () => {
+describe("<ActorDetail />", () => {
+  it("renders", () => {
     // see: https://test-utils.vuejs.org/guide/
-    cy.mount(ActorDetail)
-  })
-})
+    cy.mount(ActorDetail);
+  });
+});
