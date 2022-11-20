5bfbb7cd1174ddee220e4af5af4ac4018fc09089
Gao Xiang <gaoxiang06@kuaishou.com>
2022-11-20 16:50:21 +0800

    feat: fixture ready

--- src/components/actor/__tests__/ActorDetail.cy.js
+++ src/components/actor/__tests__/ActorDetail.cy.js
@@ -2,6 +2,9 @@ import ActorDetail from "../ActorDetail.vue";
 
 describe("<ActorDetail />", () => {
   it("renders", () => {
+    cy.intercept("/api/v1/db/data/v1/test/Actor/*", {
+      fixture: "actor.json",
+    }).as("getActor");
     // see: https://test-utils.vuejs.org/guide/
     cy.mount(ActorDetail);
   });
