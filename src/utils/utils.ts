799a55f489c4e7ffea7c03542733d76f22e3a4f7
Gao Xiang <gaoxiang06@kuaishou.com>
2023-01-07 18:09:41 +0800

    chore: change server ip

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,7 +2,7 @@ import { Api as RentalStoreApi } from "@/api/RentalStoreApi";
 
 const getRentalStoreApi = () => {
   return new RentalStoreApi({
-    baseUrl: "http://localhost:5175",
+    baseUrl: "http://localhost:5173",
   }).api;
 };
 
